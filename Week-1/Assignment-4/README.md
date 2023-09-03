de6b046805e626b054dc70eb67cca0b3af31307e
huanciou <huanciou@github.com>
2023-09-04 05:19:59 +0800

    0904

--- Week-1/Assignment-4/README.md
+++ Week-1/Assignment-4/README.md
@@ -1,6 +1,6 @@
-# version 0.0.1
-
 [HW4 Page](https://huanciou.github.io/remote-assignments/Week-1/Assignment-4/index.html)
+
+# 0902
 
 1. 大致 layout
 2. 新增 `Products` 的 `Menu`
@@ -8,3 +8,11 @@
 `建議使用 width > 1200px 收看` <br>
 `因為還沒有用好 @media screen 編輯 拉太小會變得東倒西歪的` <br>
 `調大小調的好生氣` <br>
+
+# 0904
+
+1.  - 新增廣告牆
+
+      - 每隔 1 秒會自己切換下一張 (為了能清楚演示，我把切換速度調快一點)
+
+      - 可以點擊下方圓點自行切換下一張
--- Week-1/Assignment-4/app.js
+++ Week-1/Assignment-4/app.js
@@ -2,6 +2,16 @@ const products = document.querySelector(".js-products");
 const options = document.querySelector(".js-options");
 const language = document.querySelectorAll(".js-language");
 const productsScroll = document.querySelector(".js-products-scroll");
+const ad = document.querySelectorAll(".js-ad");
+const adPlay = document.querySelector(".js-adPlay");
+const adSwitch1 = document.querySelector(".adSwitch1");
+const adSwitch2 = document.querySelector(".adSwitch2");
+const adSwitch3 = document.querySelector(".adSwitch3");
+const adSwitch4 = document.querySelector(".adSwitch4");
+
+window.onload = function () { // f5 自動 onload
+    startADs(); 
+};
 
 /* products events*/
 products.addEventListener("mouseenter", () => {
@@ -42,3 +52,83 @@ language.forEach( e => {
         e.querySelector("p").innerText = "中文－繁體 ▾";
     });
 })
+
+
+/* 利用 setInterval 來實現 定時更換 ads */
+
+    let count = 0;
+    let play = true;
+    let controlerID;
+
+    function changeAds(count){
+        ad.forEach((content, i) => { // i = [0,4) 左閉右開
+            if(i === count){
+                content.style.display = "flex";
+
+            }
+            else{
+                content.style.display = "none";
+            }
+        })
+    }
+    
+    function changeCount(){
+        count = (count+1) % ad.length;
+        changeAds(count);
+    }
+    
+    function startADs(){
+        controlerID = setInterval(changeCount, 1000); // returns 一個獨特的 "setInterval ID" 讓你後續可以操作它
+    }
+    function stopADs(){
+        clearInterval(controlerID);
+    }
+    function togglePlay(){
+        play = !play;
+        
+        if(play){
+            startADs();
+            adPlay.style.width = "8px";
+            adPlay.style.height = "8px";
+            adPlay.style.backgroundColor = "rgba(128, 128, 128, 0.6)";
+            adPlay.style.border = "1px solid rgba(128, 128, 128, 0.1)";
+            adPlay.style.marginTop = "16px";
+            adPlay.style.marginLeft = "1px";
+            adPlay.style.cursor = "pointer";
+        }
+        else{
+            stopADs();
+            adPlay.style.width = "0";
+            adPlay.style.height = "0";
+            adPlay.style.borderTop = "4.5px solid transparent";
+            adPlay.style.borderLeft = "8px solid rgba(128, 128, 128, 0.6)";
+            adPlay.style.borderBottom = "4.5px solid transparent";
+            adPlay.style.borderRight = "none";
+            adPlay.style.backgroundColor = "transparent";
+            adPlay.style.marginTop = "15.5px";
+            adPlay.style.marginLeft = "0px";
+            adPlay.style.cursor = "pointer";
+        }
+    }
+
+    adPlay.addEventListener("click", togglePlay);
+   
+    adSwitch1.addEventListener("click", ()=>{
+        count = 0;
+        changeAds(count);
+    });
+
+    adSwitch2.addEventListener("click", ()=>{
+        count = 1;
+        changeAds(count);
+    });
+
+    adSwitch3.addEventListener("click", ()=>{
+        count = 2;
+        changeAds(count);
+    });
+
+    adSwitch4.addEventListener("click", ()=>{
+        count = 3;
+        changeAds(count);
+    });
